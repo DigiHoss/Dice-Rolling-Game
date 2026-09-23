b341c32e59e1626d6279abaf7c469d7c215575dc
digihoss <hossamelhamdi6@gmail.com>
2026-09-23 18:47:23 +0200

    feat(simulator): handle a non int input from the user by throwing an exception

--- src/com/digihoss/dice/Simulator.java
+++ src/com/digihoss/dice/Simulator.java
@@ -1,32 +1,43 @@
 package com.digihoss.dice;
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 public class Simulator {
     public  static void main() {
             Random rand = new Random();
-            int rolledNumber = rand.nextInt(6) + 1;
+            int rolledNumber;
             Scanner scanner = new Scanner(System.in);
-            System.out.println("How many dice you would like to roll?");
-            String numberOfDices = scanner.nextLine();
-            System.out.println("About to roll " + numberOfDices + " dice");
+
+            try {
+                System.out.println("How many dice you would like to roll?");
+                int numberOfDices = scanner.nextInt();
+                System.out.println("About to roll " + numberOfDices + " dice");
+                for (int i = 0; i < numberOfDices; i++) {
+                    rolledNumber = rand.nextInt(6)+1;
+                    System.out.println(display(rolledNumber));
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("You should enter a valid number");
+                System.err.println("Error : " + e);
+            }
     }
 
     static String display(int value) {
-        switch (value) {
-            case 1:
-                return "-----------\n|         |\n|    o    |\n|         |\n-----------";        
-            case 2:
-                return "-----------\n|      o  |\n|         |\n|  o      |\n-----------";   
-            case 3:
-                return "-----------\n|      o  |\n|    o    |\n|  o      |\n-----------";        
-            case 4:
-                return "-----------\n|  o   o  |\n|         |\n|  o   o  |\n-----------";  
-            case 5:
-                return "-----------\n|  o   o  |\n|    o    |\n|  o   o  |\n-----------";        
-            case 6:
-                return "-----------\n|  o   o  |\n|  o   o  |\n|  o   o  |\n-----------";  
-            default:
-                return "Not a valid number";
-        }
+        return  switch (value) {
+            case 1 
+                -> "-----------\n|         |\n|    o    |\n|         |\n-----------";        
+            case 2
+                -> "-----------\n|      o  |\n|         |\n|  o      |\n-----------";   
+            case 3
+                -> "-----------\n|      o  |\n|    o    |\n|  o      |\n-----------";        
+            case 4
+                -> "-----------\n|  o   o  |\n|         |\n|  o   o  |\n-----------";  
+            case 5
+                -> "-----------\n|  o   o  |\n|    o    |\n|  o   o  |\n-----------";        
+            case 6
+                -> "-----------\n|  o   o  |\n|  o   o  |\n|  o   o  |\n-----------";  
+            default
+                -> "Not a valid number";
+        };
     }
 }
