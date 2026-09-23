924fdf6842c498ecc8fd9413e6192da6533bdede
digihoss <hossamelhamdi6@gmail.com>
2026-09-23 19:05:51 +0200

    feat(simulator): give a user more than one chance if the input isn't an integer type in order to complete the game

--- src/com/digihoss/dice/Simulator.java
+++ src/com/digihoss/dice/Simulator.java
@@ -1,43 +1,44 @@
 package com.digihoss.dice;
+
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
-public class Simulator {
-    public  static void main() {
-            Random rand = new Random();
-            int rolledNumber;
-            Scanner scanner = new Scanner(System.in);
 
+public class Simulator {
+    public static void main() {
+        Random rand = new Random();
+        int rolledNumber;
+        Scanner scanner = new Scanner(System.in);
+        boolean appComleted = false;
+        do  {
             try {
                 System.out.println("How many dice you would like to roll?");
                 int numberOfDices = scanner.nextInt();
+                
                 System.out.println("About to roll " + numberOfDices + " dice");
                 for (int i = 0; i < numberOfDices; i++) {
-                    rolledNumber = rand.nextInt(6)+1;
+                    rolledNumber = rand.nextInt(6) + 1;
                     System.out.println(display(rolledNumber));
                 }
+                break;
             } catch (InputMismatchException e) {
-                System.out.println("You should enter a valid number");
-                System.err.println("Error : " + e);
-            }
+                System.out.println("You should enter a valid number, retry again !");       
+                scanner.next();
+            } 
+  
+        } while (!appComleted);
+
     }
 
     static String display(int value) {
-        return  switch (value) {
-            case 1 
-                -> "-----------\n|         |\n|    o    |\n|         |\n-----------";        
-            case 2
-                -> "-----------\n|      o  |\n|         |\n|  o      |\n-----------";   
-            case 3
-                -> "-----------\n|      o  |\n|    o    |\n|  o      |\n-----------";        
-            case 4
-                -> "-----------\n|  o   o  |\n|         |\n|  o   o  |\n-----------";  
-            case 5
-                -> "-----------\n|  o   o  |\n|    o    |\n|  o   o  |\n-----------";        
-            case 6
-                -> "-----------\n|  o   o  |\n|  o   o  |\n|  o   o  |\n-----------";  
-            default
-                -> "Not a valid number";
+        return switch (value) {
+            case 1 -> "-----------\n|         |\n|    o    |\n|         |\n-----------";
+            case 2 -> "-----------\n|      o  |\n|         |\n|  o      |\n-----------";
+            case 3 -> "-----------\n|      o  |\n|    o    |\n|  o      |\n-----------";
+            case 4 -> "-----------\n|  o   o  |\n|         |\n|  o   o  |\n-----------";
+            case 5 -> "-----------\n|  o   o  |\n|    o    |\n|  o   o  |\n-----------";
+            case 6 -> "-----------\n|  o   o  |\n|  o   o  |\n|  o   o  |\n-----------";
+            default -> "Not a valid number";
         };
     }
 }
